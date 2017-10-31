72d5cc3a4016d210f41a6a1e7dbff6df3df9053c
Matt Brennan <matt@153.io>
2017-10-31 14:10:31 +0000

    build and install input config

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 npm install
-make -j4
-\cp -r dist/iosevka/ttf/*.ttf ~/Library/Fonts
+make custom-config set=input design='sans v-l-italic v-i-italic v-g-opendoublestorey v-m-shortleg v-tilde-low v-asterisk-low v-caret-low v-at-short v-brace-straight'
+make -j4 custom set=input
+\cp -r dist/iosevka/ttf/iosevka-input-*.ttf ~/Library/Fonts
