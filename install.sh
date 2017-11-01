64fbe654bb66f876a64fe874c4f90516bb6895b5
Matt Brennan <matt@153.io>
2017-11-01 16:36:31 +0000

    fix filepath for install

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 npm install
 make custom-config set=input design='sans v-l-italic v-i-italic v-g-opendoublestorey v-m-shortleg v-tilde-low v-asterisk-low v-caret-low v-at-short v-brace-straight'
 make -j4 custom set=input
-\cp -r dist/iosevka/ttf/iosevka-input-*.ttf ~/Library/Fonts
+\cp -r dist/iosevka-input/ttf/iosevka-input-*.ttf ~/Library/Fonts
